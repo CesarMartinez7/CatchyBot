8aa58f989702c1d21926d7010efddc0e43cace27
Szayela <cesarwamartinez@gmail.com>
2024-12-17 23:38:25 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,32 +1,48 @@
 import telebot
 import yt_dlp
-from os import remove
+from os import remove,listdir,path
 from time import sleep
 from yt_dlp import YoutubeDL
 from random import randrange
+from io import BytesIO
+import os
 
-token = "7759974599:AAEat0xZn2z1v4J7ZoirL13lnvMFQCjXBHY"
 
-bot = telebot.TeleBot(token=token)
+print("HTTP_PROXY:", os.getenv("HTTP_PROXY"))
+print("HTTPS_PROXY:", os.getenv("HTTPS_PROXY"))
+print("NO_PROXY:", os.getenv("NO_PROXY"))
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    if call.data == "option1":
-        bot.answer_callback_query(call.id, "Opción 1 seleccionada")
-    elif call.data == "option2":
-        bot.answer_callback_query(call.id, "Opción 2 seleccionada")
+
+directorio = listdir()
+extensiones = [".mp3",".webm",".jpg"]
+
+for file in directorio:
+    if path.splitext(file)[1] in extensiones:
+        remove(f"./{file}")
+        print("Limpiando directorio 🤖🤖🤖")
+
+APITOKEN = "7759974599:AAEat0xZn2z1v4J7ZoirL13lnvMFQCjXBHY"
+
+bot = telebot.TeleBot(token=APITOKEN)
+
+
+@bot.message_handler(commands=["toto"])
+def output_file(message):
+    text = str(message.text.split(" ")[2])
+    bot.reply_to(message,text)
+
 
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-    bot.reply_to(message,"Hola👋, soy tu bot asistente de descarga mp3 y mp4 de yt  🤖. ")
+    bot.reply_to(message,"Hello you 👋, soy tu bot asistente de descarga mp3, mp4 y webm hecho con Python  🤖. ")
 
 
 
 
 @bot.message_handler(commands=["help"])
 def send_help(message):
-    bot.reply_to(message,"Puedes interactuar conmingo usando comandos 💻 \n🔽/down <urlyt>: Para descargar sonido en formato mp3, la sintaxis es /down <url>\n🔽/down4 <urlyt>: Para descargar videos en formato webm ")
+    bot.reply_to(message,"c 0867")
 
 
 @bot.message_handler(commands=["down"],content_types=["text"],func=lambda m: True)
@@ -37,6 +53,7 @@ def download_music(message):
         # Extraer el nombre del video, o el titulo
         if len(message.text) <= 4:
             bot.reply_to(message,"Demasiado corto, try again")
+
         else:
             ydl_optss = {'quiet': True,'extract_flat': True,}
             with yt_dlp.YoutubeDL(ydl_optss) as ydl:
@@ -52,7 +69,7 @@ def download_music(message):
                     'preferredquality': '192',
                 }],
                 'outtmpl': video_title,
-                'ffmpeg_location': r'C:\Users\Usuario\Downloads\ffmpeg-master-latest-win64-gpl-shared\ffmpeg-master-latest-win64-gpl-shared\bin',
+                'ffmpeg_location': r".\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin"
             }
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 ydl.download([url])
@@ -65,7 +82,48 @@ def download_music(message):
     finally:
         sleep(1)
         remove(f"./{video_title}.mp3")
-            
+
+
+
+@bot.message_handler(commands=["down2"],content_types=["text"],func=lambda m: True)
+def download_music(message):
+    texto = list(message.text.split(" "))
+    url: str = texto[1]
+    bot.reply_to(message,f"Output NamefileSong 🎵🎶: {texto[2]}")
+    bot.reply_to(message,url)
+    name_fileoutput : str = texto[2]
+    try:
+        if len(url) > 43:
+            bot.reply_to(message,"Demasiado Largo, no se pueden descargar formatos de radio o mix. 🌋")
+        elif len(url) == 43:
+            ydl_optss = {'quiet': True,'extract_flat': True,}
+            with yt_dlp.YoutubeDL(ydl_optss) as ydl:
+                info_dict = ydl.extract_info(url, download=False)  
+                video_title = info_dict.get('title', 'audio')
+                bot.reply_to(message,video_title)
+                ydl_opts = {
+                'cookies': 'cookies.txt',
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+                'outtmpl': name_fileoutput,
+                'ffmpeg_location': r".\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin"
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+                bot.reply_to(message,"Espere... 🤖🐍")
+                bot.reply_to(message,"Descargando... 🤖🐍")
+            with open(f'{name_fileoutput}.mp3', 'rb') as audio:
+                bot.send_audio(message.chat.id, audio)
+                bot.reply_to(message, "¡Descarga completadaaa!")
+    except Exception as e:
+            bot.reply_to(message, f"Ocurrió un error: {str(e)}")
+    finally:
+        sleep(1)
+        remove(f"./{name_fileoutput}.mp3")
 @bot.message_handler(commands=["down4"])
 def download_mp4(message):
     try:
@@ -89,7 +147,6 @@ def download_mp4(message):
         remove(f"./{output_filename}")
 
 
-  
 
 @bot.message_handler(func=lambda m:True)
 def echo_all(message): 
@@ -112,10 +169,23 @@ def echo_all(message):
 
 
 
+
+@bot.message_handler(content_types=["photo"])
+def obtener_imagenes(message):
+    with open("imagen_recibida.jpg","rb") as image:
+        bot.send_video(message.chat.id,image)
+
+
 bot.infinity_polling()
+
+
+
+
 
 
 """
 Es importante la descarga de la ruta de ffmpeg
 Thanks 👋
+
+
 """
